2df9e15187d9dfad662f07515f9a3e7885bbafc5
Bluetea724 <syomw724@gmail.com>
2019-09-29 05:26:27 +0000

    Updated for test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,4 @@
 class Movie < ActiveRecord::Base
-	def self.all_ratings
-		items = Array[]
-		
-		@Movie_rate= Movie.all.select('rating').distinct
-		
-		@Movie_rate.each do |movie|
-			items<<movie.rating		
-		end
-	end
+	
+	
 end
